7d2e3cabb019900d90c4588e822b07fd2cb1bc1d
Artem Borodin <temych02@gmail.com>
2021-06-02 23:54:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from GetConfigs import GetConfigs
 
 if __name__ == '__main__':
     configs = GetConfigs("attachments/mail-config.yml")
-    smtp_client = SMTPClient("mail.smtp2go.com", "e-boroda.org",
-                             "10Lafagi", "artem@e-boroda.org")
+    smtp_client = SMTPClient("mail.smtp2go.com", "YOUR-LOGIN",
+                             "YOUR-PASSWORD", "RECEIVER")
 
     smtp_client.add_text_plain(configs.text_plain)
     smtp_client.add_text_html(configs.text_html)
